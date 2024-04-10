c48b6be2fef619c71204160edebcbfac385a69d2
alexandrerosapc <alexandrerosasoares3@gmail.com>
2024-04-10 11:00:17 -0300

    Full styled components

--- src/components/ListaTarefas.js
+++ src/components/ListaTarefas.js
@@ -7,14 +7,18 @@ export default function ListaTarefas({ terminadas, setTerminadas, listaTarefas }
         setTerminadas(novoArray)
         alert(`Só  faltam ${listaTarefas.length - novoArray.length} tarefas`)
     }
-    
+
     return (
         <ContainerLista>
             {listaTarefas.map((t) => (
-                <li onClick={() => terminarTarefa(t)} className={`tarefa ${terminadas.includes(t) ? "finalizada" : ""} `} key={t}>
+                <Tarefa
+                    onClick={() => terminarTarefa(t)}
+                    key={t}
+                    finalizada={terminadas.includes(t)}
+                >
                     <ion-icon name={terminadas.includes(t) ? "checkmark-circle" : "ellipse-outline"}></ion-icon>
                     {t}
-                </li>
+                </Tarefa>
             ))}
         </ContainerLista>
     )
@@ -25,4 +29,30 @@ const ContainerLista = styled.ul`
     flex-direction: column;
     border-radius: 9px;
     overflow: hidden;
+`
+
+const Tarefa = styled.li`
+    color: ${props => props.finalizada ? "#aaaaaa" : "#303f4e"};
+    text-decoration: ${props => props.finalizada ? "line-through" : "none"};
+    padding: 22px;
+    display: flex;
+    align-items: center;
+    font-size: 22px;
+    cursor: pointer;
+    background-color: #f3f3f3;
+    &:hover {
+        color: #21c0a8;
+    }
+    &:not(:last-child) {
+        border-bottom: 1px solid #e7e7e7;
+    }
+    ion-icon {
+        flex-shrink: 0;
+        font-size: 40px;
+        margin-right: 16px;
+        color: ${props => props.finalizada ? "#78dbad" : ""};
+    }
+    &:hover ion-icon {
+        color: #21c0a8;
+    }
 `
